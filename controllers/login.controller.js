9236054558585f56991e5f4ee21d6d71be317a41
ChongLynk <tronglinhmta0611@gmail.com>
2020-08-01 10:11:11 -0400

    Kiem tra dang nhap sai khong qua 4 lan

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -6,9 +6,28 @@ module.exports.index = function(req, res){
 }
 
 module.exports.postIndex=function(req, res){
+    var cookie = parseInt(req.cookies.countLogin);
+    if(!req.cookies.countLogin){
+        res.cookie('countLogin',0);
+    }
+    else{
+        
+        
+        res.clearCookie('countLogin',cookie);
+        res.cookie('countLogin', cookie+1);
+    }
+    
     var errors =[];
     var values;
     var user = db.get('users').find({email:req.body.email}).value();
+    if(cookie>4){
+        errors.push('login wrongly more than 4 times !!!');
+        res.render('login/index',{
+            errors:errors,
+            values:req.body
+        })
+        return;
+    }
     if(!user){
         errors.push("email does not exist !!!");
         res.render('login/index',{
@@ -29,7 +48,6 @@ module.exports.postIndex=function(req, res){
 
     }
     res.cookie('userId',123);
-    console.log(typeof(user.isAdmin));
     if(user.isAdmin === true){
         
         res.render('users/index',{
